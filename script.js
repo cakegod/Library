c005e64df171aedb77ca39432ba7edd74d428a42
Cake <cakemancerr@gmail.com>
2022-05-23 14:26:08 +0200

    refactored, added local storage

--- script.js
+++ script.js
@@ -1,114 +1,157 @@
 "use strict";
+const books = [];
 
 const bookDOM = {
-  form: document.querySelector(".form"),
-  bookWrapper: document.getElementById("book-wrapper"),
-  close: document.querySelector(".close"),
-  addBookButton: document.querySelector(".plus-icon"),
-  title: document.getElementById("title"),
-  author: document.getElementById("author"),
-  pages: document.getElementById("pages"),
-  readquestion: document.getElementById("readquestion"),
+	form: document.querySelector(".form"),
+	wrapper: document.getElementById("book-wrapper"),
+	close: document.querySelector(".close"),
+	addButton: document.querySelector(".plus-icon"),
+	title: document.getElementById("title"),
+	author: document.getElementById("author"),
+	pages: document.getElementById("pages"),
+	readCheck: document.getElementById("readCheck"),
 };
 
-const bookArray = {
-  myLibrary: [],
-  bookNumber: 0,
-};
-
-bookDOM.addBookButton.addEventListener("click", () => bookDOM.form.classList.add("active"));
-bookDOM.close.addEventListener("click", () => bookDOM.orm.classList.remove("active"));
-bookDOM.form.addEventListener("submit", addBookToLibrary);
+bookDOM.addButton.addEventListener("click", () =>
+	bookDOM.form.classList.add("active")
+);
+bookDOM.close.addEventListener("click", () =>
+	bookDOM.form.classList.remove("active")
+);
+bookDOM.form.addEventListener("submit", renderBook());
 
 class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
-  readToggle(truthy) {
-    return this.read = truthy;
-  }
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
 }
 
+// Submit form, push new book to the books array, render and save locally
+bookDOM.form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const newBook = new Book(
+		bookDOM.title.value,
+		bookDOM.author.value,
+		bookDOM.pages.value,
+		bookDOM.readCheck.checked
+	);
+	books.push(newBook);
+	bookDOM.form.reset();
+	renderBook();
+	saveStateStorage();
+});
 
-
-function addBookToLibrary(e) {
-  e.preventDefault();
-  let newBook = new Book(title.value, author.value, pages.value, readquestion.checked);
-  bookArray.myLibrary.push(newBook);
-  createBook(bookArray.myLibrary[bookArray.bookNumber]);
-  bookArray.bookNumber++;
+function renderBook() {
+	clearDOM();
+	books.forEach((book) => {
+		const elements = createDOMElement();
+		addClass(elements, book);
+		addInnerText(elements, book);
+		addEvents(elements, book);
+		appendChild(elements);
+	});
 }
 
-function createBook(item) {
-  const newBook = document.createElement("div");
-  bookDOM.bookWrapper.appendChild(newBook);
-  newBook.className = "book";
-
-  const deleteBook = document.createElement("span");
-  newBook.appendChild(deleteBook);
-  deleteBook.textContent = "close";
-  deleteBook.className = "material-icons-outlined close 1";
-  deleteBook.addEventListener("click", removeBook);
-
-  const title = document.createElement("p");
-  newBook.appendChild(title);
-  title.textContent = bookArray.myLibrary[bookArray.bookNumber].title;
-  title.className = "title";
-
-  const author = document.createElement("p");
-  newBook.appendChild(author);
-  author.textContent = `by ${bookArray.myLibrary[bookArray.bookNumber].author}`;
-  author.className = "author";
-
-  const pages = document.createElement("p");
-  newBook.appendChild(pages);
-  pages.textContent = bookArray.myLibrary[bookArray.bookNumber].pages + " pages";
-  pages.className = "pages";
-
-  const read = document.createElement("button");
-  newBook.appendChild(read);
-  read.textContent = "Read";
-  readInitialStyle();
-
-  read.addEventListener("click", () => {
-    if (read.classList == "read button") {
-      read.classList.add("active");
-      read.textContent = "Read";
-      bookArray.myLibrary[bookArray.myLibrary.indexOf(item)].readToggle(true);
-
-    } else if (read.classList == "read button active") {
-      read.classList.remove("active");
-      read.textContent = "Not Read";
-      bookArray.myLibrary[bookArray.myLibrary.indexOf(item)].readToggle(false);;
-
-    }
-  }
-  );
-
-  function readInitialStyle() {
-    if (readquestion.checked === true) {
-      read.className = "read button active";
-
-    } else if (readquestion.checked === false) {
-      read.textContent = "Not Read";
-      read.className = "read button";
-    }
-  }
-
-  function removeBook() {
-    newBook.remove();
-    bookArray.myLibrary.splice(bookArray.myLibrary.indexOf(item), 1);
-    bookArray.bookNumber--;
-  }
-  formReset();
-};
-
-
-function formReset() {
-  bookDOM.form.reset();
-  bookDOM.form.classList.remove("active");
+function clearDOM() {
+	bookDOM.wrapper.textContent = "";
 }
 
+function createDOMElement() {
+	const bookContainer = document.createElement("div");
+	const title = document.createElement("p");
+	const author = document.createElement("p");
+	const pages = document.createElement("p");
+	const read = document.createElement("button");
+	const close = document.createElement("span");
+
+	return { bookContainer, title, author, pages, read, close };
+}
+
+function addClass(
+	{
+		bookContainer: bookContainer,
+		title: title,
+		author: author,
+		pages: pages,
+		read: read,
+		close: close,
+	},
+	book
+) {
+	bookContainer.classList = "book";
+	title.classList = "title";
+	author.classList = "author";
+	pages.classList = "pages";
+	close.classList = "material-icons-outlined close 1";
+	if (book.read) {
+		read.innerText = "Read";
+		read.classList = "read button active";
+	} else {
+		read.innerText = "Not Read";
+		read.classList = "read button";
+	}
+}
+
+function addInnerText(
+	{ close: close, title: title, author: author, pages: pages },
+	book
+) {
+	close.innerText = "close";
+	title.innerText = book.title;
+	author.innerText = `by ${book.author}`;
+	pages.innerText = `${book.pages} pages`;
+}
+
+function appendChild({
+	bookContainer: bookContainer,
+	title: title,
+	author: author,
+	pages: pages,
+	read: read,
+	close: close,
+}) {
+	bookContainer.appendChild(close);
+	bookContainer.appendChild(title);
+	bookContainer.appendChild(author);
+	bookContainer.appendChild(pages);
+	bookContainer.appendChild(read);
+	bookDOM.wrapper.appendChild(bookContainer);
+}
+
+function addEvents({ read: read, close: close }, book) {
+	read.addEventListener("click", () => {
+		if (book.read) {
+			book.read = false;
+			read.innerText = "Not Read";
+			read.classList = "read button";
+		} else {
+			book.read = true;
+			read.innerText = "Read";
+			read.classList = "read button active";
+		}
+		saveStateStorage();
+	});
+
+	close.addEventListener("click", () => {
+		const index = books.indexOf(book);
+		books.splice(index, 1);
+		saveStateStorage();
+		renderBook();
+	});
+}
+
+function saveStateStorage() {
+	localStorage.setItem("books", JSON.stringify(books));
+}
+
+function loadStorage() {
+	if (localStorage.getItem("books") !== null) {
+		books.push(...JSON.parse(localStorage.getItem("books")));
+		renderBook();
+	}
+}
+
+loadStorage();
